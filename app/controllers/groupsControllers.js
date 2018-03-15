b9b515b3e9153834bd5a7e1a0e2c9c55cf7b80cd
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-03-15 15:58:50 -0500

    added groups controller

---
+++ app/controllers/groupsControllers.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var connection = require('../config/connection.js');
+
+router.get('/new', function(req, res){
+    res.render('groups/new');
+});
+
+router.get('/')
+
+router.post('/create', function (req, res) {
+    var query = "SELECT * FROM groups WHERE name = ?"
+
+    connection.query(query, [req.body.name], function (err, response) {
+        console.log(response)
+        if (response.length > 0) {
+            res.send('There is already a group with that name.');
+        } else {
+
+            var query2 = "INSERT INTO groups (name) VALUES (?)"
+            connection.query(query2, [req.body.name], function (err, response) {
+
+                res.redirect('/create/members');
+            });
+        }
+    });
+});
+
+router.post('/create/users', function (req, res){
+    var query = "INSERT INTO user_groups (group_id, user_id) VALUES (?, ?)";
+})
