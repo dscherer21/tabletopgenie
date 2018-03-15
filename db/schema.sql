2b67fb25522dfa4558cc1ae6f04405ad0ff52c4e
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-03-15 14:13:12 -0500

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,7 @@ USE Imperial_Assault_db;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     primary key (id) 
 );
