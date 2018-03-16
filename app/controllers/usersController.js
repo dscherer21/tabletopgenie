6b05bd3557ff67fa1dfe3ca038f0e170b64cc88d
Richard Budek <richardbudek@Richards-MacBook-Air.local>
2018-03-16 14:19:02 -0500

    login working

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -43,6 +43,8 @@ router.post('/login', function (req, res) {
     if (response.length == 0) {
       console.log ("no such user");
       //res.redirect('/users/login-in')
+      res.send('no such user');
+      return;
     };
     console.log("user found");
     bcrypt.compare(req.body.user_password, response[0].password_hash, function (err, result) {
@@ -55,7 +57,7 @@ router.post('/login', function (req, res) {
         //req.session.company = response[0].company;
         req.session.username = response[0].name;
 
-        //res.redirect('/coupon');
+        res.redirect('/group');
       } else {
         console.log("password does not match");
         //res.redirect('/users/login-in')
@@ -66,19 +68,25 @@ router.post('/login', function (req, res) {
 
 router.post('/create', function (req, res) {
   var query = "SELECT * FROM users WHERE email = ?"
-
-  connection.query(query, [req.body.email], function (err, response) {
+console.log("/create hit");
+  var userPassword = req.body.user_password;
+  var userName = req.body.user_id;
+  var userEmail = req.body.user_email;
+  connection.query(query, [req.body.user_email], function (err, response) {
     console.log(response)
     if (response.length > 0) {
       res.send('we already have an email or username for this account')
     } else {
 
+      console.log("no such user, creating a new user");
       bcrypt.genSalt(10, function (err, salt) {
         //res.send(salt)
-        bcrypt.hash(req.body.password, salt, function (err, hash) {
-          var query = "INSERT INTO users (username, email, password_hash, company) VALUES (?, ?, ?, ?)"
+        console.log("got hash");
+        bcrypt.hash(userPassword, salt, function (err, hash) {
+          console.log("at the insert query");
+          var query = "INSERT INTO users (name, email, password_hash ) VALUES (?, ?, ? )"
 
-          connection.query(query, [req.body.username, req.body.email, hash, req.body.company], function (err, response) {
+          connection.query(query, [ userName, userEmail, hash], function (err, response) {
 
             req.session.logged_in = true;
 
@@ -86,11 +94,9 @@ router.post('/create', function (req, res) {
 
             var query = "SELECT * FROM users WHERE id = ?"
             connection.query(query, [req.session.user_id], function (err, response) {
-              req.session.username = response[0].username;
+              req.session.username = response[0].name;
               req.session.user_email = response[0].email;
-              req.session.company = response[0].company;
-
-              res.redirect('/groups')
+              res.redirect('/group')
             });
           });
         });
@@ -100,40 +106,6 @@ router.post('/create', function (req, res) {
 });
 
 
-router.post('/login', function (req, res) {
-  var query = "SELECT * FROM users WHERE email = ?"
-
-  connection.query(query, [req.body.email], function (err, response) {
-    console.log(response)
-    if (response.length > 0) {
-      res.send('we already have an email or username for this account')
-    } else {
-
-      bcrypt.genSalt(10, function (err, salt) {
-        //res.send(salt)
-        bcrypt.hash(req.body.password, salt, function (err, hash) {
-          var query = "INSERT INTO users (username, email, password_hash, company) VALUES (?, ?, ?, ?)"
-
-          connection.query(query, [req.body.username, req.body.email, hash, req.body.company], function (err, response) {
-
-            req.session.logged_in = true;
-
-            req.session.user_id = response.insertId; //only way to get id of an insert for the mysql npm package
-
-            var query = "SELECT * FROM users WHERE id = ?"
-            connection.query(query, [req.session.user_id], function (err, response) {
-              req.session.username = response[0].username;
-              req.session.user_email = response[0].email;
-              req.session.company = response[0].company;
-
-              res.redirect('/coupons')
-            });
-          });
-        });
-      });
-    }
-  });
-});
 
 
 module.exports = router;
