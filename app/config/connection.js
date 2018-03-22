5cd6c60a7849956412ddf5f2d2c1f5758209ad9f
David Scherer-ODell <dscherer21@gmail.com>
2018-03-22 10:47:20 -0500

    stuf

--- app/config/connection.js
+++ app/config/connection.js
@@ -14,7 +14,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        port:3306
+        port:3306,
         host: 'localhost',
         user: 'root',
         password: '',
